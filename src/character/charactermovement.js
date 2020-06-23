053f0ffc383d59beade8c64bb3e07f0368e7b1f0
Filipe Mota <drete457@gmail.com>
2020-06-23 12:30:06 +0100

    adjust in the speed and size of the character

--- src/character/charactermovement.js
+++ src/character/charactermovement.js
@@ -1,31 +1,45 @@
 class Character {
+  constructor(image) {
+    //character position and image on the screen
+    this.image = image;
+    this.inicialPositionX = 45;
+    this.inicialPositionY = height - height / 3;
 
-    constructor(image) {
-        this.image = image;
-        this.sizeX = 58;
-        this.sizeY = 65;
-        this.characterWidth = width / 9 ;
-        this.characterHeigth = height / 6;
-       // this.characterWidth = width / 9;
-        //this.characterHeigth = height / 6;
-        this.x = this.sizeX;
-        this.y = this.sizeY * 11;
-        this.sizeXMax = this.sizeX;
-        this.sizeYMax = this.sizeY * 8;
-    }
+    //size of the character on the screen
+    this.characterWidth = width / 9;
+    this.characterHeigth = height / 3 ;
 
-    drawCharacter() {
-        image(this.image, 20, height - height / 5, this.characterWidth, this.characterHeigth,
-            this.x,
-            this.y,
-            this.sizeX, this.sizeY);
-         this.animation();
-    }
+    //size of the character in the file
+    this.sizeX = 58;
+    this.sizeY = 65;
 
-    animation() {
-        console.log(this.x)
-        this.x < this.sizeYMax-this.sizeX ? this.x += this.sizeX+6 : this.x = 0;
-    
-    }
+    //reference whit sprite use of the character file
+    this.x = this.sizeX;
+    this.y = this.sizeY * 11;
+
+    //the full sprite size will be use for the caracter movement
+    this.sizeXMax = this.sizeX;
+    this.sizeYMax = this.sizeY * 8;
+  }
+
+  drawCharacter() {
+    image(
+      this.image,
+      this.inicialPositionX,
+      this.inicialPositionY, //1º x,y -> position on the screen
+      this.characterWidth,
+      this.characterHeigth, //2º x,y -> size in the screen
+      this.x,
+      this.y, //3º x,y -> reference to the image
+      this.sizeX,
+      this.sizeY,
+    ); //4º x,y -> position of the image
+    this.animation();
+  }
+
+  animation() {
+    this.x < this.sizeYMax - this.sizeX
+      ? (this.x += this.sizeX + 6)
+      : (this.x = 0);
+  }
 }
-
